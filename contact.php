7c9550edb21e9917bcc4c5a864c7451c669746f8
Kara Ebrahim <kara.ebrahim@gmail.com>
2013-12-23 17:51:49 -0500

    fixing typo on contact page

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@
                     <h3>From I-95, North or South</h3>
                     <p>Enter the Maine Turnpike and exit the highway at Exit 48, Portland/ Westbrook. Turn right at Riverside Street, travel approximately 1 mile, and turn right at 1039 Riverside Street. Turn left onto Tee Drive.</p>
                     <h3>From downtown Portland</h3>
-                    <p>Head southwest on Congress Street aproximately 0.4 miles, and then turn right at Forest Avenue. After 4.6 miles, turn right at Riverside Street, and after 0.7 miles, turn right at 1039 Riverside Street. Turn left onto Tee Drive.</p>
+                    <p>Head southwest on Congress Street approximately 0.4 miles, and then turn right at Forest Avenue. After 4.6 miles, turn right at Riverside Street, and after 0.7 miles, turn right at 1039 Riverside Street. Turn left onto Tee Drive.</p>
                     <small>To find via GPS, enter 1039 Riverside Street, Portland, ME 04103</small>
                     <iframe width="500" height="350" frameborder="2" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=7+tee+drive+portland+me&amp;ie=UTF8&amp;hq=&amp;hnear=Tee+Dr,+Portland,+Cumberland,+Maine+04103&amp;gl=us&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=43.710084,-70.312432&amp;output=embed"></iframe>
                     <br />
